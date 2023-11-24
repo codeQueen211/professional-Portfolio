dcbe86b7b19a55b665ff76752603f004ea18e1e0
sarahOmar <117687177+codeQueen211@users.noreply.github.com>
2023-11-24 23:23:40 +0200

    Add files via upload

---
+++ display.js
@@ -0,0 +1,36 @@
+function scrollToSection() {
+    // Get the section element by its ID
+    const section = document.getElementById('section1'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToContact() {
+    // Get the section element by its ID
+    const section = document.getElementById('section2'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToEdu() {
+    // Get the section element by its ID
+    const section = document.getElementById('section3'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToSkills() {
+    // Get the section element by its ID
+    const section = document.getElementById('section4'); 
+    // Scroll to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  function openProj1(){
+    window.open('https://codequeen211.github.io/Quiz-Game-/')
+  }
+  function openProj2(){
+    window.open('https://codequeen211.github.io/Countdown-App/')
+  }
+  function openProj3(){
+    window.open('https://codequeen211.github.io/Buttons-Demo/')
+  }
+  function openProj4(){
+    window.open('https://codequeen211.github.io/My-Math-Web/')
+  }
